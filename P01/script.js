8d5ca0874e301ae5f0c22d7e6f968cc9afe0f3a5
shalkon <67655441+shalkon@users.noreply.github.com>
2020-08-19 01:55:22 +0500

    Update script.js
    Added functionality to check email validaton, check length validaion and passwords match validaton

--- P01/script.js
+++ P01/script.js
@@ -20,9 +20,13 @@ function showSuccess(input) {
 }
 
 // Function to check is email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if ( re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input, `Please provide a valid email`)
+    }
 }
 
 // Function to check if required fields have data
@@ -37,15 +41,37 @@ inputArray.forEach(function(input) {
   });
 }
 
+// Function to check Length of input field
+function checkLength(input, min, max) {
+    if ( input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at-least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input,`${getFieldId(input)} needs to be less-than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 // Function to get the id of the input field with proper case
 function getFieldId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input, input2) {
+    if ( input.value !== input2.value ) {
+        showError(input2,"Passwords don't match")
+    }
+}    
 
 // This is an event listener for the form on submit
 form.addEventListener('submit',function(e) {
     e.preventDefault();
   
     checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
 
 })
